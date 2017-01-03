449bc0c59de48ddcf39653e7400b9a39f671241f
lgyori <lg.lajos@gmail.com>
2017-01-03 16:32:31 +0100

    last change today

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -183,7 +183,7 @@
             this.dataGridView1.DataSource = this.szamlakFejeBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(12, 52);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(802, 268);
+            this.dataGridView1.Size = new System.Drawing.Size(794, 268);
             this.dataGridView1.TabIndex = 3;
             // 
             // szamlakFejeBindingSource
