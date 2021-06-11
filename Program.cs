679bac80f7efcae150f059f3c52a6130e47abe2c
Lajos GYORI <lg.lajos@gmail.com>
2021-06-11 17:41:49 +0100

    Revert "Update Program.cs"
    This reverts commit ea6f90285728a975bb3af205a16f1fe6b82022e1.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@ namespace Tamas02
 {
     static class Program
     {
+        /// <summary>
         /// The main entry point for the application.
+        /// </summary>
         [STAThread]
         static void Main()
         {
